b69cd7567c7d8fe1f20f8e8c429a1bcaeb76b888
unknown <ngonguyenducmanh@gmail.com>
2023-11-16 14:01:38 +0700

    Update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // nav
 document.addEventListener("DOMContentLoaded", function () {
     var navbar = document.querySelector('.navbar');
-
+    
     window.addEventListener('scroll', function () {
         if (window.scrollY > 50) {
             navbar.classList.add('navbar-scroll');
