b2c724ec70c90f5d21f02e13b1cf39880711575f
maxsimus77 <ngonguyenducmanh@gmail.com>
2023-11-16 10:10:36 +0700

    Push code

---
+++ script.js
@@ -0,0 +1,12 @@
+// nav
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add('navbar-scroll');
+        } else {
+            navbar.classList.remove('navbar-scroll');
+        }
+    });
+});
