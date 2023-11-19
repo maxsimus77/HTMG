e946438114c9eae85c0eb75345588f601ba8e2e5
maxsimus77 <ngonguyenducmanh@gmail.com>
2023-11-19 09:48:18 +0700

    Half final

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ document.addEventListener("DOMContentLoaded", function () {
         } else {
             navbar.classList.remove('navbar-scroll');
         }
+
+        if (window.scrollY > 1000) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
     });
 });
+
+function scrollToElement(elementId) {
+    var element = document.getElementById(elementId);
+    element.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Cuộn lên đầu trang khi nhấp vào nút cuộn
+function scrollToTop() {
+    var boxSelection = document.querySelector('.box-selection');
+    boxSelection.scrollIntoView({ behavior: 'smooth' });
+}
